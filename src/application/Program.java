c416f00e8499c60323cf140d83f3839bdd964dd1
Mayara Silva <alohasp@outlook.com>
2023-11-28 16:02:18 -0300

    Declared lambda expression

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.Scanner;
+import java.util.function.Predicate;
 
 import entities.Product;
 
@@ -11,6 +13,8 @@ public class Program {
 	public static void main(String[] args) {
 		
 		Locale.setDefault(Locale.US);
+		
+		Scanner sc = new Scanner(System.in);
 		List <Product> list = new ArrayList<>();
 		
 		list.add(new Product("TV", 900.00));
@@ -18,10 +22,15 @@ public class Program {
 		list.add(new Product("Tablet", 350.00));
 		list.add(new Product("HD Case", 80.90));
 		
-		list.removeIf(Product::nonStaticProductPredicate);
+		double min = 100.0;
+		
+		Predicate <Product> pred = p -> p.getPrice() >= min;
+		
+		list.removeIf(pred);
 		
 		for(Product prod :list) {
 			System.out.println(prod);
 		}
+		sc.close();
 	}
 }
